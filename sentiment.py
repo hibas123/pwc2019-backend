207982d4977a0fc3c816abbe593e568d5189a248
Fabian <gitkraken@fabianstamm.de>
2019-12-06 19:06:26 +0100

    Optimizing dependencies

--- sentiment.py
+++ sentiment.py
@@ -2,39 +2,39 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from json import dumps, loads
 import threading
 import re
-from textblob import TextBlob
-import tweepy
+# from textblob import TextBlob
+# import tweepy
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 nltk.download('vader_lexicon')
 
 
 class TwitterClient(object):
-    ''' 
-    Generic Twitter Class for sentiment analysis. 
+    '''
+    Generic Twitter Class for sentiment analysis.
     '''
 
     def clean_tweet(self, tweet):
-        ''' 
-        Utility function to clean tweet text by removing links, special characters 
-        using simple regex statements. 
+        '''
+        Utility function to clean tweet text by removing links, special characters
+        using simple regex statements.
         '''
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])(\w+:\/\/\S+)", " ", tweet).split())
 
-    def get_tweet_sentiment(self, tweet):
-        ''' 
-        Utility function to classify sentiment of passed tweet 
-        using textblob's sentiment method 
-        '''
-        # create TextBlob object of passed tweet text
-        analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
-        if analysis.sentiment.polarity > 0:
-            return 'positive'
-        elif analysis.sentiment.polarity == 0:
-            return 'neutral'
-        else:
-            return 'negative'
+#     def get_tweet_sentiment(self, tweet):
+#         '''
+#         Utility function to classify sentiment of passed tweet
+#         using textblob's sentiment method
+#         '''
+#         # create TextBlob object of passed tweet text
+#         analysis = TextBlob(self.clean_tweet(tweet))
+#         # set sentiment
+#         if analysis.sentiment.polarity > 0:
+#             return 'positive'
+#         elif analysis.sentiment.polarity == 0:
+#             return 'neutral'
+#         else:
+#             return 'negative'
 
 
 api = TwitterClient()
